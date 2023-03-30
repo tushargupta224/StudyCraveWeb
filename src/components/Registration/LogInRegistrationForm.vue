05c3c1cabd184873fd3022d05d4b6a4df90a2ef7
Tushar Gupta <tushargupta.hash@gmail.com>
2023-03-30 15:35:53 +0530

    🎉 feat(LogInRegistrationForm.vue): add LogInRegistrationForm component This commit adds a new component called LogInRegistrationForm.vue. The component contains a form for user registration and login. It uses the Naive UI library for the card and message provider components, and the AnimXYZ library for the transition effect. The component also imports two child components, PhoneNumberInput.vue and OtpInput.vue, which are used for phone number input and OTP input respectively. The component uses the Vuex store for authentication.

---
+++ src/components/Registration/LogInRegistrationForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <n-card
+    style="width: 600px"
+    :bordered="false"
+    size="huge"
+    role="dialog"
+    aria-modal="true"
+  >
+    <n-message-provider>
+      <XyzTransition appear mode="out-in">
+        <div xyz="fade right-80%" v-if="showPhoneInput">
+          <PhoneNumberInput @onSubmit="showPhoneInput = false" />
+        </div>
+
+        <div xyz="fade left-80%" v-if="!showPhoneInput">
+          <OtpInput @onOtpSubmit="onSubmitOtp" />
+        </div>
+      </XyzTransition>
+    </n-message-provider>
+  </n-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { NCard, NMessageProvider } from "naive-ui";
+import { XyzTransition } from "@animxyz/vue3";
+import PhoneNumberInput from "@/components/Registration/PhoneNumberInput.vue";
+import OtpInput from "@/components/Registration/OtpInput.vue";
+import { useAuthStore } from "../../stores/auth";
+
+export default defineComponent({
+  setup() {
+    const authStore = useAuthStore();
+
+    return { authStore };
+  },
+  data() {
+    return {
+      showModal: false,
+      showPhoneInput: true,
+    };
+  },
+  components: {
+    NCard,
+    NMessageProvider,
+    XyzTransition,
+    PhoneNumberInput,
+    OtpInput,
+  },
+  methods: {
+    onSubmitOtp() {
+      //
+    },
+  },
+});
+</script>
