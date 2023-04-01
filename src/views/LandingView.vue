bfecdf730d6b3b940f1f0ff3041345ab030993fc
Kunal Kishore <kunalkishore1807@gmail.com>
2023-04-01 17:30:56 +0530

    feat: Added header illustration-

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -8,14 +8,30 @@
         Join a community of learners, achieve more.
       </div> -->
     </div>
+    <div class="img-container">
+      <img :src='headerIllustration' alt="hero-img">
+
+    </div>
   </div>
 </template>
+<script lang="ts">
+import headerIllustration from '@/assets/images/Landing Page Illustration.png';
+
+export default ({
+  data(){
+    return{
+      headerIllustration:headerIllustration,
+    }
+  }
+})
+</script>
 
 <style lang="scss" scoped>
 @import "../assets/scss/variables.scss";
 
 .hero-section {
   height: 100vh;
+  display: flex;
 
   &-heading {
     max-width: 650px;
@@ -41,5 +57,59 @@
 }
 .right-side-wrapper {
   margin-top: 100px;
+}
+.img-container{
+  max-width: 80%;
+}
+.img-container img{
+  
+  width:40rem;
+  height: 40rem;
+}
+
+@media screen and (max-width: 920px){
+  .hero-section{
+    // flex-direction: column-reverse;
+    margin-top: 30px;
+    &-heading{
+      font-size: 1.8rem;
+    }
+  }
+  .right-side-wrapper{
+    margin-top: 2rem;
+  }
+  
+  .img-container img{
+    width: 30rem;
+    height: 30rem;
+  }
+}
+
+@media screen and (max-width:660px){
+  .img-container img{
+    width: 25rem;
+    height: 25rem;
+  }
+}
+@media screen and (max-width: 468px){
+  .hero-section{
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+
+    &-heading{
+      font-size: 2rem;
+    }
+  }
+  .right-side-wrapper{
+    margin-top: -0.7rem;
+  }
+  .img-container img{
+    max-width: 100%;
+    max-height: 80%;
+    width: 21rem;
+    height: 21rem;
+  }
+
 }
 </style>
