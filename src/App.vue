ce4f971559709ebe03e605805d3c696e36e3eaf3
Kunal Kishore <kunalkishore1807@gmail.com>
2023-04-01 17:31:32 +0530

    feat: Updated Global theme for naive ui

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,42 @@
 import { RouterView } from "vue-router";
 import NavBar from "./components/NavBar.vue";
 import { NDialogProvider } from "naive-ui";
+import { NConfigProvider, type GlobalThemeOverrides } from "naive-ui";
+
+const themeOverrides: GlobalThemeOverrides = {
+  Button: {
+    textColorGhostHover: "#001f3f",
+    rippleColor: "#001f3f",
+    borderHover: "1px solid #001f3f",
+  },
+  Input: {
+    borderFocus: "1px solid #001f3f",
+    borderHover: "1px solid #001f3f",
+  },
+};
 
 export default {
+  data() {
+    return {
+      themeOverrides: themeOverrides,
+    };
+  },
   components: {
     NavBar,
     NDialogProvider,
+    NConfigProvider,
   },
 };
 </script>
 
 <template>
-  <n-dialog-provider>
-    <NavBar />
+  <n-config-provider :theme-overrides="themeOverrides">
+    <n-dialog-provider>
+      <NavBar />
 
-    <RouterView />
-  </n-dialog-provider>
+      <RouterView />
+    </n-dialog-provider>
+  </n-config-provider>
 </template>
 
 <style scoped></style>
