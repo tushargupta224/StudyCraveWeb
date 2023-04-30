d4e7a1fa184213533fa922a36528ba04278cf378
Kunal Kishore <kunalkishore1807@gmail.com>
2023-04-30 15:19:17 +0530

    🎨 style(CreateChannelForm.vue): reformat code and apply consistent styling 🚀 feat(HomeView.vue): add create channel modal The CreateChannelForm.vue file has been reformatted to apply consistent styling and improve readability. The HomeView.vue file now includes a create channel modal that allows users to create a new channel. This modal is triggered by a button click and displays the CreateChannelForm component.

--- src/components/Channels/CreateChannelForm.vue
+++ src/components/Channels/CreateChannelForm.vue
@@ -1,85 +1,189 @@
 <template>
-    <n-card class="n-card">
-        <div class="form-heading">
-            <h3>✨ Create your Channel</h3>
-        </div>
-        <div class="form-subheading">
-            How does you want your channel to look like?
-        </div>
-
-        <div class="form-container">
-            <label for="channel-name">Channel's Name</label>
-            <input type="text" id="channel-name" name="channel-name" required>
-
-            <label for ='participants'>Number of participants</label>
-            <input type="number" id="particiapnts" name="participants" required min="1">
-
-            <label for="description">Channel's Description</label>
-            <input type="text" id="description" name="description">
-
-            
-        </div>
-        <div>
-            <button class="form-btn">Create Channel</button>
-        </div>
-    </n-card>
-
+  <n-card class="n-card">
+    <div class="form-heading">
+      <h3>✨ Create your Channel</h3>
+    </div>
+    <div class="form-subheading">
+      How does you want your channel to look like?
+    </div>
+    <n-form
+      ref="formRef"
+      :label-width="80"
+      :model="formValue"
+      :rules="rules"
+      :size="size"
+    >
+      <n-form-item label="Channel's Name" path="channel.name">
+        <n-input v-model:value="formValue.channel.name" />
+      </n-form-item>
+      <n-form-item label="Topic" path="channel.topic">
+        <n-input v-model:value="formValue.channel.topic" />
+      </n-form-item>
+      <n-form-item label="Description" path="channel.description">
+        <n-input
+          v-model:value="formValue.channel.description"
+          type="textarea"
+        />
+      </n-form-item>
+      <n-form-item
+        label="Max. Member Limit"
+        :validation-status="inputMaxParticipantStatus"
+      >
+        <n-input-number
+          v-model:value="formValue.channel.maxMembersLimit"
+          :min="25"
+          :max="500"
+        />
+        <template #feedback>
+          {{ inputMaxParticipantFeedback }}
+        </template>
+      </n-form-item>
+      <n-form-item>
+        <n-button @click="handleClick">Create Channel</n-button>
+      </n-form-item>
+    </n-form>
+  </n-card>
 </template>
 
-
 <script lang="ts">
-import {defineComponent} from 'vue';
-import {NCard} from 'naive-ui';
+import { defineComponent, ref } from "vue";
+import {
+  NCard,
+  NForm,
+  NFormItem,
+  NInput,
+  NInputNumber,
+  NButton,
+  type FormItemRule,
+} from "naive-ui";
+import type { FormInst } from "naive-ui";
+import type Channel from "@/types/channels/channel";
 
-
-export default defineComponent ({
-    components:{
-        NCard,
-        
+export default defineComponent({
+  setup() {
+    const formRef = ref<FormInst | null>(null);
+    return {
+      formRef,
+      size: ref<"small" | "medium" | "large">("medium"),
+      formValue: ref({
+        channel: <Partial<Channel>>{
+          name: "",
+          topic: "",
+          description: "",
+          maxMembersLimit: 100,
+        },
+      }),
+      rules: {
+        channel: {
+          name: {
+            required: true,
+            validator(rule: FormItemRule, value: string) {
+              if (!value || value.length == 0) {
+                return new Error("Channel name is required");
+              } else if (value.length < 10) {
+                return new Error(
+                  "Channel name should be of atleast 10 characters"
+                );
+              }
+              return true;
+            },
+            trigger: "blur",
+          },
+          topic: {
+            required: true,
+            validator(rule: FormItemRule, value: string) {
+              if (!value || value.length == 0) {
+                return new Error("Topic name is required");
+              } else if (value.length < 10) {
+                return new Error(
+                  "Topic name should be of atleast 8 characters"
+                );
+              }
+              return true;
+            },
+            trigger: "blur",
+          },
+          description: {
+            required: true,
+            message: "Description is required",
+            trigger: "blur",
+          },
+        },
+      },
+    };
+  },
+  computed: {
+    inputMaxParticipantStatus() {
+      if (!this.formValue.channel.maxMembersLimit) {
+        return "error";
+      }
     },
-    
-})
-
-
-
+    inputMaxParticipantFeedback() {
+      if (!this.formValue.channel.maxMembersLimit) {
+        return "Setting up of max participants is mandatory";
+      }
+    },
+  },
+  methods: {
+    handleClick(e: MouseEvent) {
+      e.preventDefault();
+      this.formRef?.validate((error) => {
+        if (!error) {
+          console.log("Valid");
+        } else {
+          console.log("error");
+        }
+      });
+    },
+  },
+  components: {
+    NCard,
+    NForm,
+    NFormItem,
+    NInput,
+    NInputNumber,
+    NButton,
+  },
+});
 </script>
 
 <style lang="scss" scoped>
-.n-card{
-    display: flex;
-    flex-direction: column;
-    width: 500px;
-    border-radius: 25px;
+.n-card {
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+  border-radius: 25px;
 }
-.form-heading h3{
-    font-size: 1.8rem;
-    margin-bottom: 5px;
+.form-heading h3 {
+  font-size: 1.8rem;
+  margin-top: -4px;
+  margin-bottom: 5px;
 }
-.form-subheading{
-    position: relative;
+.form-subheading {
+  position: relative;
+  font-size: 0.8rem;
+  font-weight: 400;
+  font-style: italic;
+  margin-bottom: 12px;
+}
+.form-container {
+  display: flex;
+  flex-direction: column;
+  & label {
+    margin-top: 12px;
     font-size: 0.8rem;
-    font-weight: 400;
-    font-style: italic;
+    font-weight: 600;
+  }
+  & input {
+    width: 400px;
+    height: 35px;
+    border: 1px solid black;
+    border-radius: 10px;
+    outline: none;
+    font-size: 1rem;
+    font-weight: 500;
     padding: 0 2%;
-}
-.form-container{
-    display: flex;
-    flex-direction: column;
-    & label{
-        margin-top: 12px;
-        font-size: 0.8rem;
-        font-weight: 600;
-    }
-    & input{
-        width: 400px;
-        height: 35px;
-        border: 1px solid black;
-        border-radius: 10px;
-        outline: none;
-        font-size: 1rem;
-        font-weight: 500;
-        padding: 0 2%;
-    }
+  }
 }
 
 input::-webkit-outer-spin-button,
@@ -89,26 +193,24 @@ input::-webkit-inner-spin-button {
 }
 
 /* Firefox */
-input[type=number] {
+input[type="number"] {
   -moz-appearance: textfield;
   appearance: textfield;
 }
 
-.form-btn{
-    cursor: pointer;
-    position: relative;
-    background: #FDD199;
-    border: 1px solid #FDD199;
-    padding: 2%;
-    border-radius: 12px;
-    float: right;
-    margin-top: 15px;
-    right: 8%;
+.form-btn {
+  cursor: pointer;
+  position: relative;
+  background: #fdd199;
+  border: 1px solid #fdd199;
+  padding: 2%;
+  border-radius: 12px;
+  float: right;
+  margin-top: 15px;
+  right: 8%;
 }
-.form-btn:hover{
-    transform: scale(1.04);
-    box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+.form-btn:hover {
+  transform: scale(1.04);
+  box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
 }
-
-
 </style>
