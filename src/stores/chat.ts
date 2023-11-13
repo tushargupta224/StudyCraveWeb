7753bd7f735a375fa48716b3d35a810ffefc3fbc
Tushar gupta <tushargupta.hash@gmail.com>
2023-11-13 13:39:03 +0530

    feat: added create quiz functionality

--- src/stores/chat.ts
+++ src/stores/chat.ts
@@ -15,6 +15,8 @@ import { db } from "../config/firebase";
 import type Channel from "../types/channels/channel";
 import type Message from "../types/channels/message";
 import { useAuthStore } from "./auth";
+import type Quiz from "../types/channels/quiz/quiz";
+import { MessageTypeEnum } from "../enum/message.enum";
 
 export const useChatStore = defineStore({
   id: "chat",
@@ -81,6 +83,28 @@ export const useChatStore = defineStore({
         senderAvatar: user.profilePic ?? "",
         senderName: (user.firstName ?? "") + " " + (user.lastName ?? ""),
         createdAt: new Date().toISOString(),
+        type: MessageTypeEnum.MESSAGE,
+      };
+
+      await addDoc(
+        collection(db, `channels/${this.channel.id}/${messageCollection}`),
+        newMessage
+      );
+    },
+    async createQuiz(messageCollection: string, quiz: Quiz) {
+      if (!this.channel) return;
+
+      const { user } = useAuthStore();
+      if (!user) return;
+
+      const newMessage: Partial<Message> = {
+        message: "",
+        senderId: user.id,
+        senderAvatar: user.profilePic ?? "",
+        senderName: (user.firstName ?? "") + " " + (user.lastName ?? ""),
+        createdAt: new Date().toISOString(),
+        quiz: quiz,
+        type: MessageTypeEnum.QUIZ,
       };
 
       await addDoc(
